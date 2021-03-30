c017f64e316bc92bb3dadf9147bb0fb56690fa65
Luka <luka.signe@outlook.com>
2021-03-30 15:04:43 +0200

    feat(game list): WIP

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {GameService} from "../game.service";
-import {Game} from "../game";
+import {Filter, Game, Sort} from "../game";
 import {Observable} from "rxjs";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-game-list',
@@ -11,16 +12,45 @@ import {Observable} from "rxjs";
 export class GameListComponent implements OnInit {
 
   gameList: Game[];
+
+  filterForm: FormGroup = new FormGroup({
+    age: new FormControl(undefined, [Validators.min(0)]),
+    nombreJoueurs: new FormControl(undefined),
+    theme: new FormControl(undefined),
+    editeur: new FormControl(undefined),
+  });
+
+
   constructor(public gameService: GameService) { }
 
   ngOnInit(): void {
     this.getGameList();
   }
 
-  getGameList(): void{
-    this.gameService.getGameList().subscribe(
+  getGameList(sort?: Sort, filter?: Filter, filterParam?: string): void{
+    this.gameService.getGameList(sort, filter, filterParam).subscribe(
       (gameList: Game[]) => this.gameList = gameList
     )
+  }
+
+  filter(): void{
+    
+  }
+
+  get age(){
+    return this.filterForm.get('age');
+  }
+
+  get nombreJoueurs(){
+    return this.filterForm.get('nombreJoueurs');
+  }
+
+  get theme(){
+    return this.filterForm.get('theme');
+  }
+
+  get editeur(){
+    return this.filterForm.get('editeurs');
   }
 
 }
