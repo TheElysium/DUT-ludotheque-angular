87ed7c0750e649c80b98f5c0f70015418d26f9ee
Theo FOURNIEZ <theofrnpro@gmail.com>
2021-03-30 21:53:31 +0200

    feat: working game details

--- src/app/game-details/game-details.component.ts
+++ src/app/game-details/game-details.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Game} from "../game";
+import {ActivatedRoute, Router} from "@angular/router";
+import {GameService} from "../game.service";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-game-details',
@@ -7,12 +10,27 @@ import {Game} from "../game";
   styleUrls: ['./game-details.component.css']
 })
 export class GameDetailsComponent implements OnInit {
-  @Input() game: Game;
+  game: Game;
+  gameId: number;
 
-  mecaniques;
-  constructor() { }
+  loading = false;
+
+  constructor(public route: ActivatedRoute, public gameService: GameService, public messageService: MessageService, public router: Router) { }
 
   ngOnInit(): void {
+    this.gameId = +this.route.snapshot.paramMap.get('id');
+    this.loading = true;
+    this.gameService.getGameById(this.gameId).subscribe(
+      game => {
+        this.loading = true;
+        this.game = game;
+        console.log('Game details : ' + game);
+      },
+      (error) => {
+        this.messageService.add({severity: 'error', summary: 'Erreur', detail: 'L\'obtention des détails du jeu échouée'});
+        this.router.navigate(['/home']);
+      }
+    );
   }
 
 
