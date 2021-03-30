aea17ddc06888d9cf6c790978f77c6782fe043fc
Theo FOURNIEZ <theofrnpro@gmail.com>
2021-03-30 22:44:04 +0200

    feat: updated fields of the Game model

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,8 +1,8 @@
  export class Game{
 
   constructor(public id: number,public nom: string,public  description: string,public  regles: string,public  langue: string,
-              public url_media: string, public  age: number,public  poids: number,public  nombreJoueurs: number,
-              public categorie: string, public duree: string, public theme_id:number, public editeur_id:number) {}
+              public url_media: string, public  age: number,public  poids: number,public  nombre_joueurs: number,
+              public categorie: string, public duree: string, public theme_id: theme_id, public editeur_id: editeur_id) {}
 
   //Create game from JSON
   public static fromJson(json: Object): Game{
@@ -15,7 +15,7 @@
       json['url_media'],
       json['age'],
       json['poids'],
-      json['nombreJoueurs'],
+      json['nombre_joueurs'],
       json['categorie'],
       json['duree'],
       json['theme_id'],
@@ -37,3 +37,30 @@ export enum Sort{
   note,
   nom,
 }
+
+ export interface theme_id{
+  id: number;
+  nom: string;
+  }
+
+ export interface tarif{
+   prixMax: number;
+   prixMin: number;
+   prixMoyen: number;
+ }
+
+ export interface editeur_id{
+  id: number;
+  nom: string;
+ }
+
+ export interface statistiques{
+  noteMax: number;
+  noteMin: number;
+  noteMoyenne: number;
+  nbCommentaires: number;
+  nbCommentairesTotal: number;
+  rang: number;
+  nbJeuxTheme: number;
+ }
+
